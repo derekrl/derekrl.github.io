2e996fc8730713372d4ac18a2fe5cc37ede1ec92
derekrl <118derek@gmail.com>
2021-03-12 22:39:16 -0700

    yall got any of that tls

--- lesson10/js/api-test.js
+++ lesson10/js/api-test.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=c52a08839dea168bd8ac1f197c2f6a78&units=imperial";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=c52a08839dea168bd8ac1f197c2f6a78&units=imperial";
 
 fetch(apiURL)
   .then((response) => response.json())
